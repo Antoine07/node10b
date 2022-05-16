ce19c895ae9ec653b7572a85f5cf865b25b0b380
Antoine07 <antoine.lucsko@gmail.com>
2022-05-16 15:30:50 +0200

    express session

--- semaine_02_approfondissement/03_template_modules_complements_techniques_J3/01_template_pug/Exercices/server.js
+++ semaine_02_approfondissement/03_template_modules_complements_techniques_J3/01_template_pug/Exercices/server.js
@@ -1,37 +1,69 @@
-import express from 'express';
-
-// __dirname
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import session from "express-session";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 8080;
-const hostname = 'localhost';
-
+const hostname = "localhost";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: "register123",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views') );
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 const posts = [
-    { title : "Pug", date : "2022-04-21", priority : "important" },
-    { title : "Express", date : "2022-04-22", priority : "normal"},
-    { title : "Node.js", date : "2022-04-23",  priority : "important"},
-  ];
-
-const authors = [
-    "alan",
-    "alice",
+  { title: "Pug", date: "2022-04-21", priority: "important" },
+  { title: "Express", date: "2022-04-22", priority: "normal" },
+  { title: "Node.js", date: "2022-04-23", priority: "important" },
 ];
 
+const authors = ["alan", "alice"];
 
-app.get('/', (req, res) => {
+// Encodage des données envoyées en POST
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-    res.render('posts/index', { posts, authors });
+app.get("/", (req, res) => {
+  console.log(req.session);
+  
+  res.render("posts/home", {
+    posts: posts.filter((p) => p.priority === "important"),
+    authors,
+  });
+});
+
+app.get("/posts", (req, res) => {
+  res.render("posts/index", { posts, authors });
+});
+
+app.get("/search", (req, res) => {
+  console.log(req.session);
+  res.render("authors/search", { authors });
+});
+
+app.post("/search", (req, res) => {
+  const { name } = req.body;
+
+  if (authors.filter((a) => a === name.trim()).length > 0) {
+    req.session.success = true;
+    res.redirect("/");
+
+    return;
+  }
+
+  req.session.success = false;
+  res.redirect("/search");
 });
 
 app.listen(port, () => {
-    console.log(`Server app listening at http://${hostname}:${port}`)
-})
+  console.log(`Server app listening at http://${hostname}:${port}`);
+});
