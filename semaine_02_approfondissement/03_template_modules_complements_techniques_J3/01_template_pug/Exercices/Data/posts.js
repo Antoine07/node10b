7f62ab49dd6fb697a6cf18e7529bc99b78712f8a
Antoine07 <antoine.lucsko@gmail.com>
2022-05-16 17:25:48 +0200

    pug

---
+++ semaine_02_approfondissement/03_template_modules_complements_techniques_J3/01_template_pug/Exercices/Data/posts.js
@@ -0,0 +1,5 @@
+export default [
+    { title: "Pug", date: "2022-04-21", priority: "important" },
+    { title: "Express", date: "2022-04-22", priority: "normal" },
+    { title: "Node.js", date: "2022-04-23", priority: "important" },
+  ];
--- semaine_02_approfondissement/03_template_modules_complements_techniques_J3/01_template_pug/Exercices/server.js
+++ semaine_02_approfondissement/03_template_modules_complements_techniques_J3/01_template_pug/Exercices/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import session from "express-session";
 import path from "path";
+import flash from 'connect-flash';
 import { fileURLToPath } from "url";
+import 'dotenv/config'; 
+
+import authors from './Data/authors.js';
+import posts from './Data/posts.js';
 
 const app = express();
-const port = 8080;
-const hostname = "localhost";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const { APP_LOCALHOST:hostname, APP_PORT:port } = process.env ;
 
 app.use(
   session({
@@ -15,26 +19,29 @@ app.use(
     saveUninitialized: true,
   })
 );
+app.use(flash());
+
+app.use((req, res, next) => {
+    // faire quelque chose si ça ne marche pas je te redirige vers une page, l'histoire s'arrête là 
+    // si cela se passe bien on passe à la suite
+
+    res.locals.flash_message = req.flash("success_message")[0]
+
+    console.log(res.locals);
+
+    next();
+})
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
-
-const posts = [
-  { title: "Pug", date: "2022-04-21", priority: "important" },
-  { title: "Express", date: "2022-04-22", priority: "normal" },
-  { title: "Node.js", date: "2022-04-23", priority: "important" },
-];
-
-const authors = ["alan", "alice"];
 
 // Encodage des données envoyées en POST
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  console.log(req.session);
-  
+
   res.render("posts/home", {
     posts: posts.filter((p) => p.priority === "important"),
     authors,
@@ -46,7 +53,7 @@ app.get("/posts", (req, res) => {
 });
 
 app.get("/search", (req, res) => {
-  console.log(req.session);
+  
   res.render("authors/search", { authors });
 });
 
@@ -54,13 +61,15 @@ app.post("/search", (req, res) => {
   const { name } = req.body;
 
   if (authors.filter((a) => a === name.trim()).length > 0) {
-    req.session.success = true;
+    req.flash("success_message", { type : "success", content : `L'auteur ${name} existe.` } );
+
     res.redirect("/");
 
     return;
   }
 
-  req.session.success = false;
+  req.flash("success_message", { type : "danger", content : `L'auteur ${name} n'existe pas.` });
+
   res.redirect("/search");
 });
 
